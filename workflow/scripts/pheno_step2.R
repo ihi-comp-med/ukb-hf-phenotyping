6c40bf7352881125960d7ca4225e95440aac055c
Albert Henry <albert.henry.16@ucl.ac.uk>
2020-10-23 21:33:11 +0100

    fix pheno_step2 scripts

--- workflow/scripts/pheno_step2.R
+++ workflow/scripts/pheno_step2.R
@@ -8,24 +8,26 @@ library(tidyverse)
 library(vroom)
 
 # for interactive / testing
-# config <- yaml::read_yaml("workflow/config.yaml")
+# config <- yaml::read_yaml("config.yaml")
 # input <- list(pheno_step1 = "results/pheno_step1.tsv",
-#               lvef = "resources/data/lvef.txt")
-# output <- "results/pheno_step2.tsv"
+#               lvef = "resources/data/lvef_derived.txt")
+# output <- "results/ukb_hf_pheno_derived.txt"
 
-df_pheno_step1 <- vroom(input$pheno_step1, delim = "\t") %>% 
+df_pheno_step1 <- vroom(input$pheno_step1, delim = "\t") %>%
   mutate(secondaryHF = ifelse(MI == 1 | congHD == 1 | valveHD == 1, 1, 0))
 
 # check descriptive stats
 
-df_lvef <- vroom(input$lvef, delim = "\t")
+df_lvef <- vroom(input$lvef, delim = "\t",
+                 col_select = config$data_spec$lvef %>% .[str_detect(names(.), "^col")] %>% as.character) %>%
+           set_names(names(config$data_spec$lvef) %>% .[str_detect(., "^col")] %>% str_remove_all("^col_"))
 
-df <- inner_join(df_pheno_step1, df_lvef, by = "eid")
+df <- left_join(df_pheno_step1, df_lvef, by = "eid")
 
 # perform step 2 phenotyping
-df_pheno_step2 <- df %>% 
-  mutate(LVSD = ifelse(lvef < 50 | LVSD == 1, 1, 0)) %>% 
-  mutate(across(MI:secondaryHF, ~replace_na(.x, 0) %>%  as.logical)) %>% 
+df_pheno_step2 <- df %>%
+  mutate(LVSD = ifelse(lvef < 50 | LVSD == 1, 1, 0)) %>%
+  mutate(across(MI:secondaryHF, ~replace_na(.x, 0) %>%  as.logical)) %>%
   mutate(pheno1 = case_when(HF ~ "case",
                             !HF ~ "control",
                             TRUE ~ "exclude"),
@@ -47,7 +49,7 @@ df_pheno_step2 <- df %>%
                             HF & !secondaryHF & !LVSD ~ "exclude",
                             !secondaryHF & LVSD ~ "case",
                             !HF & !secondaryHF & !LVSD ~ "control",
-                            TRUE ~ "exclude")) %>% 
+                            TRUE ~ "exclude")) %>%
   mutate(across(MI:secondaryHF, as.numeric))
 
 # write to output
