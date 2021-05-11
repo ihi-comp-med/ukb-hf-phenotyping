1a627d34bd84c882908a65bcc2b799bb1845cc23
Albert Henry <albert.henry.16@ucl.ac.uk>
2021-05-11 17:21:40 +0100

    change vroom to data.table and fix minor bug in pheno_step2.R

--- workflow/scripts/pheno_step2.R
+++ workflow/scripts/pheno_step2.R
@@ -5,29 +5,35 @@ config <- snakemake@config
 output <- snakemake@output[[1]]
 
 library(tidyverse)
-library(vroom)
+library(data.table)
+# library(vroom)
 
 # for interactive / testing
 # config <- yaml::read_yaml("config.yaml")
-# input <- list(pheno_step1 = "results/pheno_step1.tsv",
+# input <- list(pheno_step1 = "results/pheno_step1.txt",
 #               lvef = "resources/data/lvef_derived.txt")
 # output <- "results/ukb_hf_pheno_derived.txt"
 
-df_pheno_step1 <- vroom(input$pheno_step1, delim = "\t") %>%
+df_pheno_step1 <- fread(input$pheno_step1) %>%
   mutate(secondaryHF = ifelse(MI == 1 | congHD == 1 | valveHD == 1, 1, 0))
 
 # check descriptive stats
+df_lvef <- fread(input$lvef,
+                 select = config$data_spec$lvef %>% .[str_detect(names(.), "^col")] %>% as.character)
+setnames(df_lvef, names(config$data_spec$lvef) %>% .[str_detect(., "^col")] %>% str_remove_all("^col_"))
 
-df_lvef <- vroom(input$lvef, delim = "\t",
-                 col_select = config$data_spec$lvef %>% .[str_detect(names(.), "^col")] %>% as.character) %>%
-           set_names(names(config$data_spec$lvef) %>% .[str_detect(., "^col")] %>% str_remove_all("^col_"))
+df <- df_lvef[df_pheno_step1, on = "eid"]
 
-df <- left_join(df_pheno_step1, df_lvef, by = "eid")
+# df_lvef <- vroom(input$lvef, delim = "\t",
+#                  col_select = config$data_spec$lvef %>% .[str_detect(names(.), "^col")] %>% as.character) %>%
+#            set_names(names(config$data_spec$lvef) %>% .[str_detect(., "^col")] %>% str_remove_all("^col_"))
+#
+# df <- left_join(df_pheno_step1, df_lvef, by = "eid")
 
 # perform step 2 phenotyping
 df_pheno_step2 <- df %>%
   mutate(LVSD = ifelse(lvef < 50 | LVSD == 1, 1, 0)) %>%
-  mutate(across(MI:secondaryHF, ~replace_na(.x, 0) %>%  as.logical)) %>%
+  mutate(across(HF:secondaryHF, ~replace_na(.x, 0) %>%  as.logical)) %>%
   mutate(pheno1 = case_when(HF ~ "case",
                             !HF ~ "control",
                             TRUE ~ "exclude"),
@@ -54,8 +60,8 @@ df_pheno_step2 <- df %>%
                             HF & !LVSD ~ "exclude",
                             !HF & !LVSD ~ "control",
                             TRUE ~ "exclude")) %>%
-  mutate(across(MI:secondaryHF, as.numeric))
+  mutate(across(HF:secondaryHF, as.numeric))
 
 # write to output
 dir.create(dirname(output), showWarnings = F, recursive = T)
-vroom_write(df_pheno_step2, output, delim = "\t")
+fwrite(df_pheno_step2, output, sep = "\t")
